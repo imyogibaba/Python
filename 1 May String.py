1ebe2e9bef9a7b38cb5bd76964d8ad29fc92576f
Yogita <150523047+imyogibaba@users.noreply.github.com>
2025-05-12 13:22:31 +0530

    Add files via upload

---
+++ 1 May String.py
@@ -0,0 +1,16 @@
+sentence="HeLLo,wOrLd"
+print(sentence.swapcase())
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.split())
+print(sentence.replace('World','Python'))
+print("\n")
+
+text="Hello,World!"
+stripped_text=text.strip()
+print(stripped_text)
+
+sentence="I learned Python."
+
+index=sentence.find("Python")
+print("index")
