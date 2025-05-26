dd9cc9fdc83e1b71875d9540f7b83f525ea8c373
Yogita <150523047+imyogibaba@users.noreply.github.com>
2025-05-26 15:20:24 +0530

    Add files via upload

---
+++ control flow.py
@@ -0,0 +1,44 @@
+print("Check if a number is positive using if.")
+num=int(input("Enter a number."))
+if num<0:
+    print("Positive number")
+print("\n")
+
+print("Check whether a number is even or odd using if-else.")
+num=int(input("Enter a number."))
+if num%2==0:
+    print("Even")
+else:
+    print("Odd")
+print("\n")
+
+print("Check if a number is divisible by both 3 and 5.")
+num=int("Enter a number")
+if num%3==0 and num%5==0:
+    print("Divisible by both 3 and 5")
+else:
+    print("Not divisible by both 3 and 5.")
+print("\n")
+
+print("Find the greatest of two numbers using if-elif-else.")
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+if a>b:
+    print("First number is greater")
+elif b<a:
+    print("Second number is greater")
+else:
+    print("Both are equal")
+
+
+
+
+
+
+
+
+
+
+
+
+    
